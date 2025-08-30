15e51de2485fc65f6aeec9bea899d130d4c254c6
asintha96 <audeshitha@gmail.com>
2025-08-31 01:41:30 +0530

    Update page.tsx
    fix: correct Wallet component state and type handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,42 @@
-"use client"
+"use client";
 
-import {getTrending} from "./lib/api"
-import { useState,useEffect } from "react"
-import React from 'react'
+import { getTrending } from "./lib/api";
+import { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 
-const page = () => {
-
-  const [trending, setTrending] = useState([]);
+export default function Page() {
+  const [trending, setTrending] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     getTrending()
-      .then((data) => {setTrending(data) 
-        console.log(data)
+      .then((data) => {
+        setTrending(data);
+        console.log(data);
       })
-     
       .catch(() => setError("Failed to load Data"))
       .finally(() => setLoading(false));
   }, []);
- 
+
   if (loading) return <p>Loading Data...</p>;
   if (error) return <p>{error}</p>;
 
-
-
-
-
   return (
     <div>
-      <Link href="/wallet/9BB6NFEcjBCtnNLFko2FqVQBq8HHM13kCyYcdQbgpump">My Wallet</Link>
-      <br></br>
-<ul>
- {trending.map(({token,pools}, index) => (<p key={index}>{token.name}----{token.symbol}------{pools[0].price.usd}------{pools[0].txns.volume}-----{pools[0].txns.volume24h}</p>))}
-</ul>
-
-
-
-      
+      <Link href="/wallet/9BB6NFEcjBCtnNLFko2FqVQBq8HHM13kCyYcdQbgpump">
+        My Wallet
+      </Link>
+      <br />
+      <ul>
+        {trending.map(({ token, pools }, index) => (
+          <li key={index}>
+            {token.name} ---- {token.symbol} ---- {pools[0].price.usd} ----{" "}
+            {pools[0].txns.volume} ---- {pools[0].txns.volume24h}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
-
-export default page
